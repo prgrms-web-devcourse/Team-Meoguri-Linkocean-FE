6c6356c89b97c834f9ef444b21c469864ee2968e
yoo chang heon <dbckdgjs1130@naver.com>
2022-07-29 02:50:53 +0900

    [#1] feat: button type, color, text, width, height를 props로 추가

--- components/common/button.tsx
+++ components/common/button.tsx
@@ -1,23 +1,91 @@
+import { color, text } from "@/styles/theme";
 import styled from "@emotion/styled";
 
-interface ButtonProps {
-  size: "small" | "large" | "line";
-  color: "main-color" | "sky-blue" | "aqua" | "gray";
-  width?: number;
-  height?: number;
-  text?: string;
-  onClick: React.MouseEventHandler<HTMLButtonElement>;
+export interface ButtonProps {
+  buttonType: "small" | "large" | "line";
+  colorType: "main-color" | "skyblue" | "aqua" | "gray";
+  width?: string;
+  height?: string;
+  buttonText?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
+const handleButton = (colorType: string, buttonType: string) => {
+  let buttonColor = "";
+  switch (colorType) {
+    case "main-color": {
+      buttonColor = color.$mainColor;
+      break;
+    }
+    case "skyblue": {
+      buttonColor = color.$skyBlue;
+      break;
+    }
+    case "aqua": {
+      buttonColor = color.$aqua;
+      break;
+    }
+    case "gray": {
+      buttonColor = color.$gray100;
+      break;
+    }
+    default:
+      return ``;
+  }
 
-const Button = ({ size, color, width, height, text, onClick }: ButtonProps) => {
-  const a = 1;
+  switch (buttonType) {
+    case "small":
+      return `
+        width: 62px;
+        height: 45px;
+        background-color:${buttonColor};
+      `;
+    case "large":
+      return `
+        width:250px;
+        height:45px;
+        background-color:${buttonColor}
+      `;
+    case "line":
+      return `
+        width:250px;
+        height:45px;
+        background-color:white;
+        border:1px solid ${buttonColor}
+      `;
+    default:
+      return ``;
+  }
+};
+
+const Button = ({
+  buttonType,
+  colorType,
+  width,
+  height,
+  buttonText,
+  onClick,
+}: ButtonProps) => {
   return (
-    <StyledButton type="button" onClick={onClick}>
-      {text}
+    <StyledButton
+      type="button"
+      buttonType={buttonType}
+      colorType={colorType}
+      onClick={onClick}
+      width={width}
+      height={height}
+    >
+      {buttonText}
     </StyledButton>
   );
 };
 
-const StyledButton = styled.button``;
+const StyledButton = styled.button<ButtonProps>`
+  border: none;
+  border-radius: 8px;
+  ${text.$subtitle1}
+  ${(props) => handleButton(props.colorType, props.buttonType)};
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+`;
 
 export default Button;
