a6f87afb01d32836eeb49797e7baae99bab22915
YuHyun <puh0128@gmail.com>
2022-07-29 16:48:56 +0900

    [#13] feat: Textarea 공통 컴포넌트 구현

---
+++ components/common/textarea.tsx
@@ -0,0 +1,34 @@
+import styled from "@emotion/styled";
+import * as theme from "@/styles/theme";
+import { forwardRef } from "react";
+
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  width?: string;
+  height?: string;
+}
+
+const Textarea = forwardRef(
+  (
+    { width = "470px", height = "155px", ...props }: TextareaProps,
+    ref?: React.Ref<HTMLTextAreaElement>
+  ) => {
+    return <StyledTextarea style={{ width, height }} {...props} ref={ref} />;
+  }
+);
+
+const StyledTextarea = styled.textarea`
+  border: 1px solid ${theme.color.$gray600};
+  border-radius: 8px;
+  padding: 10px 15px;
+  ${theme.text.$body1}
+  background-color: #fff;
+  outline: 0;
+  box-sizing: border-box;
+
+  &:focus {
+    box-shadow: 0px 0px 1px 2px ${theme.color.$skyBlue};
+  }
+`;
+
+export default Textarea;
