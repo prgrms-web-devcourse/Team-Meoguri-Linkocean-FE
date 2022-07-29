4a0e741b5fc154a8b6573c21d93896a0f53be614
Jieun Kim <67778677+jieun0411@users.noreply.github.com>
2022-07-29 18:39:19 +0900

    refactor: 파일명 변경

---
+++ stories/components/logoutTooltip.stories.tsx
@@ -0,0 +1,9 @@
+import LogoutTooltip from "@/components/common/logoutTooltip";
+
+export default {
+  title: "components/LogoutTooltip",
+  component: LogoutTooltip,
+  argTypes: {},
+};
+
+export const Default = () => <LogoutTooltip />;
