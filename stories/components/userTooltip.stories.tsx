b6d3093e060a8e91c582de8c47571e922d7fda59
Jieun Kim <67778677+jieun0411@users.noreply.github.com>
2022-07-29 17:58:42 +0900

    style: 툴팁 디자인

---
+++ stories/components/userTooltip.stories.tsx
@@ -0,0 +1,9 @@
+import UserTooltip from "@/components/common/userTooltip";
+
+export default {
+  title: "components/UserTooltip",
+  component: UserTooltip,
+  argTypes: {},
+};
+
+export const Default = () => <UserTooltip />;
