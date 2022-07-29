3a800c929966ce099422561fef9a3666d3e85118
Jieun Kim <67778677+jieun0411@users.noreply.github.com>
2022-07-29 18:39:19 +0900

    refactor: 파일명 변경

--- stories/components/userTooltip.stories.tsx
+++
@@ -1,9 +0,0 @@
-import UserTooltip from "@/components/common/userTooltip";
-
-export default {
-  title: "components/UserTooltip",
-  component: UserTooltip,
-  argTypes: {},
-};
-
-export const Default = () => <UserTooltip />;
