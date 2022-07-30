907e6382e0317e002b54eca88ad33d97cbabd673
YuHyun <puh0128@gmail.com>
2022-07-31 02:11:38 +0900

    [#12] style: open 여부에 따라 화살표 회전하도록 설정

--- components/common/select/trigger.tsx
+++ components/common/select/trigger.tsx
@@ -26,15 +26,27 @@ const Trigger = ({
 };
 
 const StyledButton = styled.button<{ open: boolean }>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   height: 44px;
   border: 1px solid ${theme.color.$gray600};
   border-radius: ${(props) => (props.open ? "8px 8px 0 0" : "8px")};
   padding: 10px 15px;
   color: ${theme.color.$gray600};
+  background-color: #fff;
   ${theme.text.$body1};
   text-align: start;
-  background: url("/icon/select-arrow.svg") no-repeat center right 15px;
   cursor: pointer;
+
+  &::after {
+    content: "";
+    display: inline-block;
+    width: 14px;
+    height: 8px;
+    background: url("/icon/select-arrow.svg") center;
+    transform: ${(props) => (props.open ? " rotate(180deg)" : null)};
+  }
 `;
 
 export default Trigger;
