ed6f86933bc478fda64018a37eaf77b82326ad6a
NamgyungKim <namgyung.kim@gmail.com>
2022-07-31 02:21:44 +0900

    [#16] style: css 수정

--- components/common/bookmarkCard/dropBox.tsx
+++ components/common/bookmarkCard/dropBox.tsx
@@ -68,15 +68,24 @@ const List = styled.ul`
     border-bottom: 7px solid ${color.$gray100};
     content: "";
   }
-
+  li:not(:last-child) button {
+    border-bottom: 1px solid ${color.$gray100};
+  }
+  li:first-child {
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+    overflow: hidden;
+  }
+  li:last-child {
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+    overflow: hidden;
+  }
   li > button {
     height: 26px;
     transition: background-color 0.3s;
     width: 100%;
     ${text.$caption}
-    &:not(:last-child) {
-      border-bottom: 1px solid ${color.$gray100};
-    }
     &:hover {
       background-color: ${color.$gray50};
     }
