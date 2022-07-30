b113ab9687732fbd543cd11c06cdad7e68af3805
Jieun Kim <67778677+jieun0411@users.noreply.github.com>
2022-07-30 15:25:30 +0900

    [#30] style: 레이아웃 디자인

---
+++ stories/components/following.stories.tsx
@@ -0,0 +1,9 @@
+import Following from "@/components/common/following";
+
+export default {
+  title: "components/Following",
+  component: Following,
+  argTypes: {},
+};
+
+export const Default = () => <Following />;
