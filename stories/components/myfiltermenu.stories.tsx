6796938c25ed95585fed8139d7a95382d0af6081
yoo chang heon <dbckdgjs1130@naver.com>
2022-07-31 03:02:30 +0900

    [#7] feat: tags, category 정보 props로 꺼내줌

--- stories/components/myfiltermenu.stories.tsx
+++ stories/components/myfiltermenu.stories.tsx
@@ -1,17 +1,97 @@
 import MyFilterMenu, {
   MyFilterMenuProps,
 } from "@/components/common/filterMenu/myFilterMenu";
+import { useState, Dispatch, SetStateAction } from "react";
 
 export default {
-  title: "Components/filterMenu",
+  title: "Components/FilterMenu",
   component: MyFilterMenu,
   argTypes: {
-    category: {
-      defaultValue: ["1", "2", "3"],
+    tagList: {
+      defaultValue: [
+        {
+          name: "JAVA",
+          count: 5,
+        },
+        {
+          name: "JAVASCRIPT",
+          count: 5,
+        },
+        {
+          name: "PYTHON",
+          count: 5,
+        },
+        {
+          name: "C++",
+          count: 5,
+        },
+        {
+          name: "C",
+          count: 5,
+        },
+        {
+          name: "C#",
+          count: 5,
+        },
+        {
+          name: "RUBY",
+          count: 5,
+        },
+        {
+          name: "GOLANG",
+          count: 5,
+        },
+      ],
+    },
+    categoryList: {
+      defaultValue: [
+        "self_development",
+        "humanities",
+        "politics",
+        "social",
+        "art",
+        "science",
+        "technology",
+        "it",
+        "home",
+        "health",
+        "travel",
+        "cooking",
+      ],
     },
   },
 };
 
-export const Default = (args: MyFilterMenuProps) => {
-  return <MyFilterMenu {...args} />;
+export const Default = ({
+  tagList,
+  categoryList,
+}: // setSelectedCategory,
+// setSelectedTag,
+MyFilterMenuProps) => {
+  const [arr1, setArr] = useState<string[]>();
+  const [category, setCategory] = useState<string>();
+  const getTags = (arr: string[]) => {
+    setArr(arr);
+    console.log(arr1);
+  };
+  const getCategory = (element: string) => {
+    setCategory(element);
+  };
+
+  return (
+    <div>
+      {arr1?.map((e) => (
+        <div>{e}</div>
+      ))}
+      {category}
+      <MyFilterMenu
+        tagList={tagList}
+        categoryList={categoryList}
+        getTags={getTags}
+        getData={getCategory}
+        // setSelectedCategory={setSelectedCategory}
+        // setSelectedTag={setSelectedTag}
+      />
+    </div>
+  );
 };
