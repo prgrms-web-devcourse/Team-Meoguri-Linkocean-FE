67cf2bb0655e59f13e40332a32eee2842635e56e
yoo chang heon <dbckdgjs1130@naver.com>
2022-07-31 15:13:42 +0900

    [#7] feat: feedFilterMenu 구현

--- components/common/filterMenu/feedFilterMenu.tsx
+++ components/common/filterMenu/feedFilterMenu.tsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import FilterBorder from "./filterBorder";
 import FilterHeader from "./filterHeader";
-import FilterFolder from "./filterFolder";
 import FilterElement from "./filterElement";
 
 interface FeedFilterMenuProps {
-  getData: (arr: string) => void;
+  getCategoryData: (arr: string) => void;
 }
 
-const FeedFilterMenu = ({ getData }: FeedFilterMenuProps) => {
+const FeedFilterMenu = ({ getCategoryData }: FeedFilterMenuProps) => {
   const [selectedCategory, setSelectedCategory] = useState<string>("");
+  //   const [selectedElement, setSelectedElement] = useState<string>("");
   const categoryList = [
     "자기계발",
     "인문",
@@ -24,24 +24,42 @@ const FeedFilterMenu = ({ getData }: FeedFilterMenuProps) => {
     "여행",
     "요리",
   ];
-  const getCategory = (element: string) => {
+  const handleCategoryClick = (element: string) => {
     setSelectedCategory(element);
-    getData(element);
+    getCategoryData(element);
   };
+
+  //   const getCategory = (element: string) => {
+  //     setSelectedCategory(element);
+  //     getCategoryData(element);
+  //   };
   return (
     <FilterBorder>
       <FilterHeader src="/icon/folder.svg" alt="category" arrow={false}>
         카테고리
       </FilterHeader>
       {categoryList
-        ? categoryList.map((element) => (
-            <FilterElement
-              title={element}
-              type="category"
-              disabled={false}
-              isSelected
-            />
-          ))
+        ? categoryList.map((element) =>
+            element === selectedCategory ? (
+              <FilterElement
+                title={element}
+                key={element}
+                type="category"
+                disabled={false}
+                onClick={() => handleCategoryClick(element)}
+                isSelected
+              />
+            ) : (
+              <FilterElement
+                title={element}
+                key={element}
+                type="category"
+                disabled={false}
+                onClick={() => handleCategoryClick(element)}
+                isSelected={false}
+              />
+            )
+          )
         : null}
     </FilterBorder>
   );
