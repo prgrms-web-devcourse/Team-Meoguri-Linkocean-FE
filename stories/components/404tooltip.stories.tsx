2d7750ea67427bbfa01617ddd0917dfa72beb423
Jieun Kim <67778677+jieun0411@users.noreply.github.com>
2022-08-01 16:10:39 +0900

    [#48] feat: 404 tooltip

--- stories/components/404tooltip.stories.tsx
+++ stories/components/404tooltip.stories.tsx
@@ -1,9 +1,29 @@
-import Tooltip404 from "@/components/common/404tooltip";
+import Tooltip404, { TooltipProps } from "@/components/common/404tooltip";
 
 export default {
   title: "components/Tooltip404",
   component: Tooltip404,
-  argTypes: {},
+  argTypes: {
+    index: {
+      control: {
+        type: "select",
+        options: [
+          "joy",
+          "nadia",
+          "hyoni",
+          "hani",
+          "crush",
+          "haha",
+          "groot",
+          "jacob",
+        ],
+        defaultValue: "joy",
+      },
+    },
+  },
 };
 
-export const Default = () => <Tooltip404 />;
+export const Default = (args: TooltipProps) => <Tooltip404 {...args} />;
+Default.args = {
+  index: "joy",
+};
