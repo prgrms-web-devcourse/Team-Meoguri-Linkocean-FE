403e90ee809354d5c3cb382032bcc3c56e6a318d
Jieun Kim <67778677+jieun0411@users.noreply.github.com>
2022-08-01 13:18:11 +0900

    [#48] style: 레이아웃 디자인

---
+++ components/common/404tooltip.tsx
@@ -0,0 +1,39 @@
+import styled from "@emotion/styled";
+import { color, text } from "@/styles/theme";
+
+const LogoutTooltip = () => {
+  return (
+    <Tooltip>
+      <Arrow />
+      <Logout />
+    </Tooltip>
+  );
+};
+
+export default LogoutTooltip;
+
+const Tooltip = styled.div``;
+
+const Arrow = styled.div`
+  width: 0;
+  height: 0;
+  margin-left: 146px;
+  border-bottom: 8px solid ${color.$skyBlue};
+  border-top: 8px solid transparent;
+  border-left: 8px solid transparent;
+  border-right: 8px solid transparent;
+`;
+
+const Logout = styled.div`
+  width: 177px;
+  height: 40px;
+  border-radius: 8px;
+  border: 3px solid ${color.$skyBlue};
+  color: ${color.$skyBlue};
+  ${text.$subtitle1};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  cursor: pointer;
+`;
---
+++ stories/components/404tooltip.stories.tsx
@@ -0,0 +1,9 @@
+import Tooltip404 from "@/components/common/404tooltip";
+
+export default {
+  title: "components/Tooltip404",
+  component: Tooltip404,
+  argTypes: {},
+};
+
+export const Default = () => <Tooltip404 />;
