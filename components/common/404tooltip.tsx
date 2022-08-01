36c0ba7c7b0b7d9f7c6a4334d28a2c5984642648
Jieun Kim <67778677+jieun0411@users.noreply.github.com>
2022-08-01 15:00:22 +0900

    [#48] style: 레이아웃 디자인 수정

--- components/common/404tooltip.tsx
+++ components/common/404tooltip.tsx
@@ -4,28 +4,30 @@ import { color, text } from "@/styles/theme";
 const LogoutTooltip = () => {
   return (
     <Tooltip>
-      <Arrow />
       <Logout />
+      <Arrow />
     </Tooltip>
   );
 };
 
 export default LogoutTooltip;
 
-const Tooltip = styled.div``;
+const Tooltip = styled.div`
+  display: position;
+`;
 
 const Arrow = styled.div`
   width: 0;
   height: 0;
-  margin-left: 146px;
-  border-bottom: 8px solid ${color.$skyBlue};
-  border-top: 8px solid transparent;
+  margin-left: 17px;
+  border-bottom: 12px solid transparent;
+  border-top: 12px solid ${color.$skyBlue};
   border-left: 8px solid transparent;
   border-right: 8px solid transparent;
 `;
 
 const Logout = styled.div`
-  width: 177px;
+  width: 81px;
   height: 40px;
   border-radius: 8px;
   border: 3px solid ${color.$skyBlue};
