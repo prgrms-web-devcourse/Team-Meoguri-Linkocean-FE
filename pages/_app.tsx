c5f83617dbbaeaae88137003638378205acafacd
YuHyun <puh0128@gmail.com>
2022-08-02 04:07:53 +0900

    [#70] feat: Session 전역 상태로 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import "@/styles/reset.css";
 import "@/styles/global.css";
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
