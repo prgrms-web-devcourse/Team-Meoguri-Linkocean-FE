588fa93f8cbc68786e92e88d0b451584a60e5ed0
YuHyun <puh0128@gmail.com>
2022-08-02 04:07:04 +0900

    [#70] feat: next-auth API route 추가

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,11 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+  ],
+});
