1acfea1cb4cdbbc7fb1e0085f02308daa2b35787
Jieun Kim <67778677+jieun0411@users.noreply.github.com>
2022-08-01 16:10:39 +0900

    [#48] feat: 404 tooltip

--- components/common/404tooltip.tsx
+++ components/common/404tooltip.tsx
@@ -1,16 +1,43 @@
 import styled from "@emotion/styled";
-import { color, text } from "@/styles/theme";
+import { text } from "@/styles/theme";
 
-const LogoutTooltip = () => {
+export type Member =
+  | "joy"
+  | "nadia"
+  | "hyoni"
+  | "hani"
+  | "crush"
+  | "haha"
+  | "groot"
+  | "jacob";
+export interface TooltipProps {
+  index: Member;
+}
+
+const LogoutTooltip = ({ index, ...props }: TooltipProps) => {
+  const { name, color } = TooltipMap[index];
   return (
     <Tooltip>
-      <Logout />
-      <Arrow />
+      <Logout color={color} {...props}>
+        {name}
+      </Logout>
+      <Arrow color={color} />
     </Tooltip>
   );
 };
 
 export default LogoutTooltip;
+
+const TooltipMap = {
+  joy: { name: "조이", color: "#DDC3EF" },
+  nadia: { name: "나디아", color: "#EBCE5C" },
+  hyoni: { name: "효니", color: "#8CC3E4" },
+  hani: { name: "하니", color: "#D77589" },
+  crush: { name: "크러쉬", color: "#9487CE" },
+  haha: { name: "하하", color: "#ACF1EF" },
+  groot: { name: "그루트", color: "#A73834" },
+  jacob: { name: "제이콥", color: "#6BD49D" },
+} as const;
 
 const Tooltip = styled.div`
   display: position;
@@ -21,7 +48,7 @@ const Arrow = styled.div`
   height: 0;
   margin-left: 17px;
   border-bottom: 12px solid transparent;
-  border-top: 12px solid ${color.$skyBlue};
+  border-top: 12px solid ${(props) => props.color};
   border-left: 8px solid transparent;
   border-right: 8px solid transparent;
 `;
@@ -30,8 +57,7 @@ const Logout = styled.div`
   width: 81px;
   height: 40px;
   border-radius: 8px;
-  border: 3px solid ${color.$skyBlue};
-  color: ${color.$skyBlue};
+  border: 3px solid ${(props) => props.color};
   ${text.$subtitle1};
   display: flex;
   justify-content: center;
