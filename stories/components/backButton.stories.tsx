80768017c51547be41d61457d5a91d61cbcfb956
NamgyungKim <87519250+NamgyungKim@users.noreply.github.com>
2022-08-02 15:41:19 +0900

    [#72] feat:backButton 구현

---
+++ stories/components/backButton.stories.tsx
@@ -0,0 +1,8 @@
+import BackButton from "@/components/common/backButton";
+
+export default {
+  title: "components/BackButton",
+  component: BackButton,
+};
+
+export const Default = () => <BackButton />;
