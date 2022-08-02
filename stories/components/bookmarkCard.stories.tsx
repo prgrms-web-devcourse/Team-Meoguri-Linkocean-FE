69badc12f4b043321d6d8c7a57ff5ec8644e3115
NamgyungKim <87519250+NamgyungKim@users.noreply.github.com>
2022-08-02 17:47:54 +0900

    [#74] style:bookmarkLayout 수정

--- stories/components/bookmarkCard.stories.tsx
+++ stories/components/bookmarkCard.stories.tsx
@@ -1,43 +1,114 @@
 import BookmarkCard from "@/components/common/bookmarkCard";
-import { BookmarkProps } from "@/components/common/bookmarkCard/bookmarkCard";
+import { Bookmark } from "@/types/model";
 
 export default {
   title: "components/bookmarkCard",
   component: BookmarkCard,
-  argTypes: {
-    data: {
-      type: "text",
-      defaultValue: {
-        favorite: false,
-        imageUrl:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfnFobXDZ9yiIjEpNUoA6CaoinBpWJF2d1rA&usqp=CAU",
-        id: 2,
-        title: "다음 웹툰",
-        tags: ["Spring", "React"],
-        category: "IT",
-        url: "https://emotion.sh/docs/best-practices",
-        updatedAt: "2022-01-01",
-        openType: "all",
-        likeCount: 12,
-        isWriter: true,
-      },
-    },
-  },
 };
 
-export const Default = (args: BookmarkProps) => (
-  <div>
-    <div style={{ display: "flex" }}>
-      <BookmarkCard {...args} />
-      <BookmarkCard {...args} />
-      <BookmarkCard {...args} />
-      <BookmarkCard {...args} />
-    </div>
-    <div style={{ display: "flex" }}>
-      <BookmarkCard {...args} />
-      <BookmarkCard {...args} />
-      <BookmarkCard {...args} />
-      <BookmarkCard {...args} />
-    </div>
-  </div>
+export const Default = () => (
+  <>
+    {cardData.map((data) => {
+      return <BookmarkCard key={data.id} data={data} />;
+    })}
+  </>
 );
+
+const cardData: Bookmark[] = [
+  {
+    id: 1,
+    title: "네이버 웹툰",
+    url: "https://comic.naver.com/index",
+    openType: "all",
+    updatedAt: "2022-01-01",
+    imageUrl:
+      "https://s.pstatic.net/static/www/mobile/edit/2016/0705/mobile_212852414260.png",
+    likeCount: 122,
+    isFavorite: false,
+    isWriter: true,
+    tags: ["Spring", "네이버", "자주사용하는"],
+  },
+  {
+    id: 2,
+    title: "emotion",
+    url: "https://emotion.sh/docs/best-practices",
+    openType: "all",
+    category: "IT",
+    updatedAt: "2022-12-02",
+    imageUrl:
+      "http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg",
+    likeCount: 222,
+    isFavorite: false,
+    isWriter: true,
+    tags: ["style", "React", "라이브러리"],
+  },
+  {
+    id: 3,
+    title: "네이버 웹툰",
+    url: "https://comic.naver.com/index",
+    openType: "all",
+    category: "건강",
+    updatedAt: "2022-02-04",
+    imageUrl:
+      "http://www.urbanbrush.net/web/wp-content/uploads/edd/2020/02/urbanbrush-20200227023608426223.jpg",
+    likeCount: 34,
+    isFavorite: false,
+    isWriter: true,
+    tags: ["Spring", "React"],
+  },
+  {
+    id: 4,
+    title: "카페추천",
+    url: "https://comic.naver.com/index",
+    openType: "all",
+    category: "가정",
+    updatedAt: "2022-02-31",
+    imageUrl:
+      "http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg",
+    likeCount: 83,
+    isFavorite: false,
+    isWriter: true,
+    tags: ["Spring", "React"],
+  },
+  {
+    id: 5,
+    title: "음원 사이트",
+    url: "https://comic.naver.com/index",
+    openType: "all",
+    category: "IT",
+    updatedAt: "2022-07-22",
+    imageUrl: "https://byline.network/wp-content/uploads/2018/05/cat.png",
+    likeCount: 64,
+    isFavorite: false,
+    isWriter: true,
+    tags: ["Spring", "React"],
+  },
+  {
+    id: 6,
+    title: "멜론",
+    url: "https://comic.naver.com/index",
+    openType: "all",
+    category: "IT",
+    updatedAt: "2022-08-23",
+    imageUrl:
+      "http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg",
+    likeCount: 134,
+    isFavorite: false,
+    isWriter: true,
+    tags: ["Spring", "React"],
+  },
+  {
+    id: 7,
+    title: "고양이",
+    url: "https://byline.network/2018/05/21-20/",
+    openType: "all",
+    category: "요리",
+    updatedAt: "2022-06-26",
+    imageUrl:
+      "http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg",
+    likeCount: 23432,
+    isFavorite: false,
+    isWriter: true,
+    tags: ["Spring", "React"],
+  },
+];
