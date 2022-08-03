8f2b10a8df3e46866003f1533841439cb554bc13
NamgyungKim <namgyung.kim@gmail.com>
2022-08-03 17:05:06 +0900

    [#80] style: style 수정

--- components/common/profileImage.tsx
+++ components/common/profileImage.tsx
@@ -4,10 +4,10 @@ type ImgSize = "lg" | "md" | "sm" | number;
 
 export interface ProfileImageProps {
   src?: string;
-  size: ImgSize;
+  size?: ImgSize;
 }
 
-const profileImage = ({ src, size = "md", ...props }: ProfileImageProps) => (
+const ProfileImage = ({ src, size = "md", ...props }: ProfileImageProps) => (
   <ProfileImg
     src={src || "/image/default-profile-image.png"}
     size={size}
@@ -34,6 +34,7 @@ const ProfileImg = styled.img<ProfileImageProps>`
   height: ${(props) => profileSize(props.size)}px;
   border-radius: 50%;
   display: inline-block;
+  background-color: #fff;
 `;
 
-export default profileImage;
+export default ProfileImage;
