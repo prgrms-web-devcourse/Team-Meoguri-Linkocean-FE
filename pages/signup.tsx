2b78cb6e5dd5d2ba77d2602526c877f4c9c5200e
YuHyun <puh0128@gmail.com>
2022-08-03 17:41:04 +0900

    [#51] feat: 회원가입 API 구현 및 메인 페이지 이동하는 방법 변경

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -8,6 +8,10 @@ import styled from "@emotion/styled";
 import Link from "next/link";
 import { ChangeEventHandler, FormEvent, useCallback, useState } from "react";
 import { usernameRegExp } from "@/utils/validation";
+import Head from "next/head";
+import * as theme from "@/styles/theme";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 type ChangeInputHandler = ChangeEventHandler<HTMLInputElement>;
 type CategoryType = typeof CATEGORY[number];
@@ -21,6 +25,10 @@ const SignUp = () => {
     value: [],
     errorText: "1개 이상 선택해주세요.",
   });
+  const [username, setUsername] = useState({ value: "", errorText: "" });
+
+  const router = useRouter();
+
   const getNextUserCategoryValue = useCallback(
     (selectedCategory: CategoryType) => {
       const hasSelectedCategory = userCategory.value.includes(selectedCategory);
@@ -31,6 +39,7 @@ const SignUp = () => {
     },
     [userCategory.value]
   );
+
   const handleCategoryClick: ChangeInputHandler = (e) => {
     const selectedCategory = e.target.name as CategoryType;
     const nextValue = getNextUserCategoryValue(selectedCategory);
@@ -39,8 +48,6 @@ const SignUp = () => {
       errorText: nextValue.length > 0 ? "" : "1개 이상 선택해주세요.",
     });
   };
-
-  const [username, setUsername] = useState({ value: "", errorText: "" });
   const handleUsernameChange: ChangeInputHandler = (e) => {
     const nextValue = e.target.value;
     setUsername({
@@ -50,82 +57,88 @@ const SignUp = () => {
         : "유저 네임은 2-6자의 한글, 영어, 숫자만 사용 가능합니다.",
     });
   };
-
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
 
-    // TODO: API 연결 및 유저 네임 중복 에러 메시지 설정
-    alert(
-      `submit\n선호 카테고리: ${userCategory.value.join(" ")}\n유저 네임: ${
-        username.value
-      }`
-    );
+    // TODO: 유저 네임 중복 에러 메시지 설정
+    // API 요청 테스트 필요
+    // signup({ username: username.value, category: userCategory.value });
+  };
+
+  const signup = async (body: {
+    username: string;
+    category: CategoryType[];
+  }) => {
+    try {
+      await axios.post(`${process.env.END_POINT}/api/v1/profiles`, body);
+      router.push("/my/favorite");
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
-    <Layout>
-      <h1>
-        <Link href="/" passHref>
-          <a>
-            <img
-              src="/icon/logo.svg"
-              alt="Link Ocean"
-              width={357}
-              height={57}
+    <>
+      <Head>
+        <title>LinkOcean | 회원가입</title>
+      </Head>
+      <Layout>
+        <h1>
+          <img src="/icon/logo.svg" alt="Link Ocean" width={357} height={57} />
+        </h1>
+
+        <Form onSubmit={handleSubmit}>
+          <CategorySection>
+            <Label style={{ marginBottom: "22px" }}>선호 카테고리 선택</Label>
+            <CategoryContainer>
+              {CATEGORY.map((category) => (
+                <CategoryItem
+                  name={category}
+                  onChange={handleCategoryClick}
+                  key={category}
+                />
+              ))}
+            </CategoryContainer>
+            <ErrorText style={{ height: "14px" }}>
+              {userCategory.errorText}
+            </ErrorText>
+          </CategorySection>
+
+          <UsernameSection>
+            <Label style={{ marginBottom: "6px" }}>유저 네임</Label>
+            <Input
+              type="text"
+              value={username.value}
+              onChange={handleUsernameChange}
             />
-          </a>
-        </Link>
-      </h1>
 
-      <Form onSubmit={handleSubmit}>
-        <CategorySection>
-          <Label style={{ marginBottom: "22px" }}>선호 카테고리 선택</Label>
-          <CategoryContainer>
-            {CATEGORY.map((category) => (
-              <CategoryItem
-                name={category}
-                onChange={handleCategoryClick}
-                key={category}
-              />
-            ))}
-          </CategoryContainer>
-          <ErrorText style={{ height: "14px" }}>
-            {userCategory.errorText}
-          </ErrorText>
-        </CategorySection>
+            <ErrorText style={{ marginTop: "5px", height: "14px" }}>
+              {username.errorText}
+            </ErrorText>
+          </UsernameSection>
 
-        <UsernameSection>
-          <Label style={{ marginBottom: "6px" }}>유저 네임</Label>
-          <Input
-            type="text"
-            value={username.value}
-            onChange={handleUsernameChange}
-          />
+          <StyledButton
+            type="submit"
+            buttonType="small"
+            colorType="main-color"
+            width="175"
+            style={{ margin: "63px auto 0" }}
+            disabled={
+              // TODO:닉네임 중복일 때 조건 추가 필요
+              userCategory.errorText !== "" ||
+              username.errorText !== "" ||
+              username.value === ""
+            }
+          >
+            회원 가입
+          </StyledButton>
 
-          <ErrorText style={{ marginTop: "5px", height: "14px" }}>
-            {username.errorText}
-          </ErrorText>
-        </UsernameSection>
-
-        <Button
-          type="submit"
-          buttonType="small"
-          colorType="main-color"
-          width="175"
-          style={{ margin: "63px auto 0" }}
-          disabled={
-            userCategory.errorText !== "" ||
-            username.errorText !== "" ||
-            username.value === ""
-          }
-        >
-          회원 가입
-        </Button>
-      </Form>
-
-      <Test>선택된 선호 카테고리: {userCategory.value.join(" ")}</Test>
-      <Test>유저 네임: {username.value}</Test>
-    </Layout>
+          <Link href="/" passHref>
+            <LinkText>이미 다른 계정이 있나요? 로그인하러 가기</LinkText>
+          </Link>
+        </Form>
+      </Layout>
+    </>
   );
 };
 
@@ -163,6 +176,18 @@ const UsernameSection = styled.section`
   justify-content: flex-start;
 `;
 
-const Test = styled.div``;
+const StyledButton = styled(Button)`
+  &:disabled {
+    background-color: ${theme.color.$mainColor};
+    cursor: not-allowed;
+  }
+`;
+
+const LinkText = styled.a`
+  display: block;
+  margin: 9px auto 0;
+  color: ${theme.color.$gray600};
+  ${theme.text.$caption};
+`;
 
 export default SignUp;
