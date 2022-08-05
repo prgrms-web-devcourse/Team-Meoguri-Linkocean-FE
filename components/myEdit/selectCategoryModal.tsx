c27db32926797123a281e19e20c53b2b235d7b56
NamgyungKim <namgyung.kim@gmail.com>
2022-08-05 23:13:31 +0900

    [#94] selectCategoryModal 컴포넌트 구현
    - 선택취소, 모달 밖을 클릭시 이전으로 돌아감
    - 카테고리를 선택 후 선택완료 클릭 시 해당 카테고리 반영
    - 아무것도 선택하지 않을 시 알림메시지가 뜬다.

---
+++ components/myEdit/selectCategoryModal.tsx
@@ -0,0 +1,148 @@
+import { color, text } from "@/styles/theme";
+import { CATEGORY } from "@/types/type";
+import styled from "@emotion/styled";
+import React, { ChangeEventHandler, useEffect, useState } from "react";
+import Button from "../common/button";
+import CategoryItem from "../common/categoryItem";
+import ErrorText from "../common/errorText";
+import Modal from "../common/modal";
+
+type ChangeInputHandler = ChangeEventHandler<HTMLInputElement>;
+
+const SelectCategoryModal = ({
+  setCategories,
+  categories,
+}: {
+  categories: string[];
+  setCategories: (categories: string[]) => void;
+}) => {
+  const [isShowModal, setIsShowModal] = useState<boolean>(false);
+  const [isShowErrorMsg, setIsShowErrorMsg] = useState(false);
+  const [favoriteCategories, setFavoriteCategories] =
+    useState<string[]>(categories);
+
+  const handleCategoryClick: ChangeInputHandler = (e) => {
+    if (favoriteCategories.includes(e.target.name)) {
+      const filtered = favoriteCategories.filter(
+        (category) => category !== e.target.name
+      );
+      setFavoriteCategories(filtered);
+      return;
+    }
+    setFavoriteCategories([...favoriteCategories, e.target.name]);
+  };
+
+  const submitCategories = () => {
+    if (favoriteCategories.length < 1) {
+      setIsShowErrorMsg(true);
+      return;
+    }
+    setIsShowErrorMsg(false);
+    setCategories(favoriteCategories);
+    setIsShowModal(false);
+  };
+
+  const cancel = () => {
+    setFavoriteCategories(categories);
+    setIsShowModal(false);
+  };
+
+  useEffect(() => {
+    console.log(favoriteCategories);
+  }, [favoriteCategories]);
+
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          setIsShowModal(true);
+        }}
+        width="120"
+        buttonType="small"
+        colorType="skyblue"
+      >
+        카테고리 선택
+      </Button>
+      <Modal
+        isShow={isShowModal}
+        setIsShow={setIsShowModal}
+        width={936}
+        height={495}
+        closed={cancel}
+      >
+        <div>
+          <CategorySection>
+            <Title>선호 카테고리 선택</Title>
+            <CategoryContainer>
+              {CATEGORY.map((category) => (
+                <CategoryItem
+                  name={category}
+                  onChange={handleCategoryClick}
+                  checked={favoriteCategories.includes(category)}
+                  // on={favoriteCategories.includes(category)}
+                  key={category}
+                />
+              ))}
+            </CategoryContainer>
+            {isShowErrorMsg ? (
+              <ErrorText style={{ height: "14px" }}>
+                1개 이상 선택해 주세요
+              </ErrorText>
+            ) : (
+              <ErrorText style={{ height: "14px" }} />
+            )}
+          </CategorySection>
+          <ButtonBox>
+            <Button
+              onClick={submitCategories}
+              width="166"
+              buttonType="large"
+              colorType="main-color"
+            >
+              선택 완료
+            </Button>
+            <Button
+              width="166"
+              buttonType="large"
+              colorType="gray"
+              onClick={cancel}
+            >
+              선택 취소
+            </Button>
+          </ButtonBox>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+const CategorySection = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 780px;
+  margin: auto;
+`;
+
+const CategoryContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 26px;
+  margin-bottom: 11px;
+`;
+
+const ButtonBox = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 16px;
+  margin-top: 50px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 40px;
+  color: ${color.$gray800};
+  text-align: center;
+  ${text.$headline5}
+`;
+
+export default SelectCategoryModal;
