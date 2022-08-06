c5297820750ddd41a61eea4b90b913255cb454d4
YuHyun <puh0128@gmail.com>
2022-08-06 19:08:39 +0900

    [#120] feat: API 모듈화

---
+++ utils/apis/notification.ts
@@ -0,0 +1,17 @@
+import { authInstance } from "./instance";
+
+export type ShareNotificationPayload = {
+  targetId: number;
+  bookmarkId: number;
+};
+
+const notificationAPI = {
+  shareNotification: (payload: ShareNotificationPayload) =>
+    authInstance.post("/notifications", payload),
+  getNotifications: (queryString: string) =>
+    authInstance.get<{ notifications: Notification[] }>(
+      `/notifications?${queryString}`
+    ),
+};
+
+export default notificationAPI;
