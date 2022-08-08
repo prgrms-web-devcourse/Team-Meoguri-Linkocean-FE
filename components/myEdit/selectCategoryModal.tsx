d1313627ee301af85f9bccd3c1ec9c31692e7a3c
NamgyungKim <namgyung.kim@gmail.com>
2022-08-08 18:15:58 +0900

    [#151] fix: selectCategoryModal 수정

--- components/myEdit/selectCategoryModal.tsx
+++ components/myEdit/selectCategoryModal.tsx
@@ -56,54 +56,56 @@ const SelectCategoryModal = ({
       >
         카테고리 선택
       </Button>
-      <Modal
-        isShow={isShowModal}
-        setIsShow={setIsShowModal}
-        width={936}
-        height={495}
-        closed={cancel}
-      >
-        <div>
-          <CategorySection>
-            <Title>선호 카테고리 선택</Title>
-            <CategoryContainer>
-              {CATEGORY.map((category) => (
-                <CategoryItem
-                  name={category}
-                  onChange={handleCategoryClick}
-                  checked={favoriteCategories.includes(category)}
-                  key={category}
-                />
-              ))}
-            </CategoryContainer>
-            {favoriteCategories.length < 1 ? (
-              <ErrorText style={{ height: "14px" }}>
-                1개 이상 선택해 주세요
-              </ErrorText>
-            ) : (
-              <ErrorText style={{ height: "14px" }} />
-            )}
-          </CategorySection>
-          <ButtonBox>
-            <Button
-              onClick={submitCategories}
-              width="166"
-              buttonType="large"
-              colorType="main-color"
-            >
-              선택 완료
-            </Button>
-            <Button
-              width="166"
-              buttonType="large"
-              colorType="gray"
-              onClick={cancel}
-            >
-              선택 취소
-            </Button>
-          </ButtonBox>
-        </div>
-      </Modal>
+      {isShowModal ? (
+        <Modal
+          isShow={isShowModal}
+          setIsShow={setIsShowModal}
+          width={936}
+          height={495}
+          closed={cancel}
+        >
+          <div>
+            <CategorySection>
+              <Title>선호 카테고리 선택</Title>
+              <CategoryContainer>
+                {CATEGORY.map((category) => (
+                  <CategoryItem
+                    name={category}
+                    onChange={handleCategoryClick}
+                    on={favoriteCategories.includes(category)}
+                    key={category}
+                  />
+                ))}
+              </CategoryContainer>
+              {favoriteCategories.length < 1 ? (
+                <ErrorText style={{ height: "14px" }}>
+                  1개 이상 선택해 주세요
+                </ErrorText>
+              ) : (
+                <ErrorText style={{ height: "14px" }} />
+              )}
+            </CategorySection>
+            <ButtonBox>
+              <Button
+                onClick={submitCategories}
+                width="166"
+                buttonType="large"
+                colorType="main-color"
+              >
+                선택 완료
+              </Button>
+              <Button
+                width="166"
+                buttonType="large"
+                colorType="gray"
+                onClick={cancel}
+              >
+                선택 취소
+              </Button>
+            </ButtonBox>
+          </div>
+        </Modal>
+      ) : null}
     </div>
   );
 };
