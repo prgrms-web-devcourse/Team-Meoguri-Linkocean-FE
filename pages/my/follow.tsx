94759f347c3f6d5af2746cd75e6f9cf5d1f549b8
YuHyun <puh0128@gmail.com>
2022-08-09 02:12:33 +0900

    [#149] feat: 마이페이지 팔로워/팔로잉 무한 스크롤

--- pages/my/follow.tsx
+++ pages/my/follow.tsx
@@ -10,9 +10,13 @@ import { useCallback, useEffect, useState } from "react";
 import { Profile } from "@/types/model";
 import profileAPI from "@/utils/apis/profile";
 import { useRouter } from "next/router";
+import useIntersectionObserver from "@/hooks/useIntersectionObserver";
+import { getQueryString } from "@/utils/queryString";
 
 // TODO: 팔로우 하기, 언팔로우 하기, 전역 유저 정보, 라우팅
 const PAGE_SIZE = 8;
+const isLastCard = (index: number, length: number) =>
+  index === Math.max(0, length - 1);
 
 type Filtering = {
   tab: "follower" | "followee";
@@ -30,26 +34,63 @@ const Follow = () => {
   const [tempUser, setTempUser] = useState(DUMMY_USER.HYONI); // 유저 context로 대체될 코드
 
   const [state, setState] = useState(INITIAL_FILTERING);
-  const [followProfiles, setFollowProfiles] = useState<Profile[]>([]);
+  const [followProfiles, setFollowProfiles] = useState<{
+    value: Profile[];
+    isLoading: boolean;
+  }>({ value: [], isLoading: false });
+  const [isEndPage, setIsEndPage] = useState(false);
 
   const handleChange = (type: string, value: string | number) => {
-    setState({ ...state, [type]: value });
+    setState({ ...state, [type]: value, page: INITIAL_FILTERING.page });
+    setIsEndPage(false);
   };
 
   const getFollowProfiles = useCallback(async () => {
     const { profileId } = tempUser;
     const { tab, ...query } = state;
-    const queryString = Object.entries(query)
-      .map((entry) => entry.join("="))
-      .join("&");
+    const queryString = getQueryString(query);
 
     try {
-      const response = await profileAPI.getFollow(profileId, tab, queryString);
-      setFollowProfiles(response.data.profiles);
+      setFollowProfiles(({ value }) => ({ value, isLoading: true }));
+
+      const {
+        data: { profiles },
+      } = await profileAPI.getFollow(profileId, tab, queryString);
+
+      if (profiles.length === 0 || profiles.length < query.size) {
+        setIsEndPage(true);
+      }
+
+      setFollowProfiles(({ value }) => {
+        const nextValue =
+          query.page === INITIAL_FILTERING.page
+            ? profiles
+            : [...value, ...profiles];
+
+        return {
+          value: nextValue,
+          isLoading: false,
+        };
+      });
     } catch (error) {
       console.error(error);
     }
   }, [state, tempUser]);
+
+  const onIntersect: IntersectionObserverCallback = ([{ isIntersecting }]) => {
+    if (isEndPage) {
+      setTarget(undefined);
+      return;
+    }
+
+    if (isIntersecting && !followProfiles.isLoading) {
+      setState({ ...state, page: state.page + 1 });
+    }
+  };
+  const { setTarget } = useIntersectionObserver({
+    onIntersect,
+    threshold: 0.8,
+  });
 
   useEffect(() => {
     getFollowProfiles();
@@ -77,28 +118,6 @@ const Follow = () => {
         </PageLayout.Aside>
         <PageLayout.Article>
           <Layout>
-            <Test>{JSON.stringify(state, null, " ")}</Test>
-            <Test>
-              {new URLSearchParams(
-                Object.entries(state).map(([key, value]) => [
-                  key,
-                  value.toString(),
-                ])
-              ).toString()}
-            </Test>
-            <button
-              type="button"
-              onClick={() => {
-                setTempUser(
-                  tempUser.username === "효니"
-                    ? DUMMY_USER.MEOGURI
-                    : DUMMY_USER.HYONI
-                );
-              }}
-            >
-              {tempUser.username === "효니" ? "머구리" : "효니"}로 변경
-            </button>
-
             <Form>
               <FollowRadio
                 name="follow"
@@ -119,15 +138,24 @@ const Follow = () => {
                 }}
               />
             </Form>
+
             <FollowCardContainer>
-              {followProfiles.map(
-                ({ profileId, imageUrl, isFollow, username }) => (
-                  <Following
-                    profileImg={imageUrl}
-                    userName={username}
-                    following={isFollow}
-                    key={profileId}
-                  />
+              {followProfiles.value.map(
+                ({ profileId, imageUrl, isFollow, username }, index) => (
+                  <div
+                    ref={
+                      isLastCard(index, followProfiles.value.length)
+                        ? setTarget
+                        : null
+                    }
+                  >
+                    <Following
+                      profileImg={imageUrl}
+                      userName={username}
+                      following={isFollow}
+                      key={profileId}
+                    />
+                  </div>
                 )
               )}
             </FollowCardContainer>
@@ -159,24 +187,22 @@ export const FollowCardContainer = styled.div`
   width: 835px;
 `;
 
-const Test = styled.div``;
-
 export default Follow;
 
 const DUMMY_USER = {
   HYONI: {
-    profileId: 6,
+    profileId: 3,
     imageUrl: null,
     favoriteCategories: ["여행"],
     username: "효니",
     bio: null,
-    followerCount: 1,
-    followeeCount: 1,
+    followerCount: 4,
+    followeeCount: 0,
     tags: [],
     categories: [],
   },
   MEOGURI: {
-    profileId: 18,
+    profileId: 5,
     imageUrl: null,
     favoriteCategories: ["IT"],
     username: "머구리",
