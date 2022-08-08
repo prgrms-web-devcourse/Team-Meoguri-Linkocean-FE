b7c25a8d30257fbd4fbe3b054131e537dce0e231
NamgyungKim <namgyung.kim@gmail.com>
2022-08-09 02:37:33 +0900

    [#132] feat: feed detail API 연결

--- pages/feed/detail/[bookmarkId].tsx
+++ pages/feed/detail/[bookmarkId].tsx
@@ -2,13 +2,30 @@ import FeedFilterMenu from "@/components/common/filterMenu/feedFilterMenu";
 import PageLayout from "@/components/common/pageLayout";
 import DetailPage from "@/components/detail";
 import { getBookMarkDetail } from "@/types/dummyData";
-import { useState } from "react";
+import bookmarkAPI from "@/utils/apis/bookmark";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const MyDetail = () => {
+  const router = useRouter();
   const [category, setCategory] = useState<string>();
   const getCategory = (element: string) => {
     setCategory(element);
   };
+  const [bookmarkData, setBookmarkData] = useState(getBookMarkDetail);
+
+  useEffect(() => {
+    if (!router.isReady) return;
+    const id = Number(router.query.bookmarkId);
+    (async () => {
+      try {
+        const { data } = await bookmarkAPI.getBookmarkDetail(id);
+        setBookmarkData(data);
+      } catch (e) {
+        console.log(e);
+      }
+    })();
+  }, [router.query, router.isReady]);
 
   return (
     <PageLayout>
@@ -16,7 +33,7 @@ const MyDetail = () => {
         <FeedFilterMenu getCategoryData={getCategory} />
       </PageLayout.Aside>
       <PageLayout.Article>
-        <DetailPage data={getBookMarkDetail} isWriter />
+        <DetailPage data={bookmarkData} isWriter />
       </PageLayout.Article>
     </PageLayout>
   );
