1331f3310b6cc8781debb81e7c20f447fc5c5d6c
NamgyungKim <namgyung.kim@gmail.com>
2022-08-09 12:18:46 +0900

    [#157] cardDelete시 callback함수 실행

--- components/common/bookmarkCard/bookmarkCard.tsx
+++ components/common/bookmarkCard/bookmarkCard.tsx
@@ -7,6 +7,7 @@ import DropBox from "./dropBox";
 
 export interface BookmarkProps {
   data: Bookmark;
+  deleteBookmark: (id: number) => void;
 }
 
 const OPEN_TYPE = {
@@ -15,7 +16,7 @@ const OPEN_TYPE = {
   private: "비공개",
 };
 
-const BookmarkCard = ({ data }: BookmarkProps) => {
+const BookmarkCard = ({ data, deleteBookmark }: BookmarkProps) => {
   const {
     category,
     url,
@@ -63,7 +64,7 @@ const BookmarkCard = ({ data }: BookmarkProps) => {
         </div>
         <div>
           <Star favorite={isFavorite} />
-          <DropBox isWriter={isWriter} id={id}>
+          <DropBox deleteBookmark={deleteBookmark} isWriter={isWriter} id={id}>
             <More />
           </DropBox>
         </div>
