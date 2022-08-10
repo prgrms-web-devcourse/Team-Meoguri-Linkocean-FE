c2cbb1c75d00c9ff6d86ba6543497162db07ae2a
YuHyun <puh0128@gmail.com>
2022-08-10 23:48:46 +0900

    [#204] feat: NoResult 공통 컴포넌트 구현

---
+++ components/common/noResult.tsx
@@ -0,0 +1,37 @@
+import styled from "@emotion/styled";
+import * as theme from "@/styles/theme";
+
+const NoResult = () => {
+  return (
+    <Container>
+      <ImageContainer>
+        <img src="image/crush.png" alt="검색 결과가 없습니다!" aria-hidden />
+      </ImageContainer>
+      <Text>검색 결과가 없습니다!</Text>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 835px;
+  height: 420px;
+  opacity: 0.7;
+`;
+
+const ImageContainer = styled.div`
+  transform: rotate(-9deg);
+  img {
+    display: block;
+  }
+`;
+
+const Text = styled.span`
+  ${theme.text.$headline5}
+  color: ${theme.color.$mainColor};
+`;
+
+export default NoResult;
