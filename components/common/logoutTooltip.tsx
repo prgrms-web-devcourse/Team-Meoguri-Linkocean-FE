aaa578298f7928854aaca10e5ff4703fd0fd5398
NamgyungKim <87519250+NamgyungKim@users.noreply.github.com>
2022-08-11 17:31:32 +0900

    [#227] feat: logout기능 추가

--- components/common/logoutTooltip.tsx
+++ components/common/logoutTooltip.tsx
@@ -1,17 +1,29 @@
 import styled from "@emotion/styled";
 import { color, text } from "@/styles/theme";
 import Link from "next/link";
+import storage from "@/utils/localStorage";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/router";
 
 const LogoutTooltip = ({ ...props }) => {
+  const router = useRouter();
+
   const handleLogout = () => {
-    // logout
+    try {
+      storage.removeItem("OAUTH_TYPE");
+      storage.removeItem("LINKOCEAN_TOKEN");
+      signOut();
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <Tooltip {...props}>
       <Arrow />
       <Link href="/" passHref>
-        <Logout onClick={() => handleLogout}>logout</Logout>
+        <Logout onClick={handleLogout}>logout</Logout>
       </Link>
     </Tooltip>
   );
