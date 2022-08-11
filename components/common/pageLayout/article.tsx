545c4e236295fb6744c1252d6223519094e4340c
NamgyungKim <87519250+NamgyungKim@users.noreply.github.com>
2022-08-11 18:00:09 +0900

    [#217] style: 여백 변경

--- components/common/pageLayout/article.tsx
+++ components/common/pageLayout/article.tsx
@@ -8,7 +8,7 @@ const Article = ({ children }: PageLayoutProps) => {
 const ArticleBox = styled.article`
   background-color: white;
   width: 100%;
-  padding: 100px 50px;
+  padding: 80px 50px;
 `;
 
 export default Article;
