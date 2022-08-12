f308cd8c2347762b019c9247f058a7227abf9a08
yoo chang heon <dbckdgjs1130@naver.com>
2022-08-12 23:35:58 +0900

    chore: storybook 삭제

--- stories/components/backButton.stories.tsx
+++
@@ -1,8 +0,0 @@
-import BackButton from "@/components/common/backButton";
-
-export default {
-  title: "components/BackButton",
-  component: BackButton,
-};
-
-export const Default = () => <BackButton />;
--- stories/components/logoutTooltip.stories.tsx
+++
@@ -1,9 +0,0 @@
-import LogoutTooltip from "@/components/common/logoutTooltip";
-
-export default {
-  title: "components/LogoutTooltip",
-  component: LogoutTooltip,
-  argTypes: {},
-};
-
-export const Default = () => <LogoutTooltip />;
