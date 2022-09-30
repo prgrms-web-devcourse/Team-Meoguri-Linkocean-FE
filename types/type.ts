3f1d269ac975495c2a6adf2c7db11802894b7b22
YuHyun <puh0128@gmail.com>
2022-09-30 12:43:04 +0900

    [#397] feat: 북마크 목록 필터 타입 추가

--- types/type.ts
+++ types/type.ts
@@ -13,6 +13,37 @@ export const CATEGORY = [
   "요리",
 ] as const;
 
+export const FilterType = {
+  favorite: "즐겨찾기",
+  tag: "태그",
+  category: "카테고리 ",
+} as const;
+export type FilterKeyType = keyof typeof FilterType;
+
+export const SortType = {
+  upload: "최신 순",
+  like: "좋아요 순",
+} as const;
+export type SortKeyType = keyof typeof SortType;
+
+export interface BaseQueryType {
+  searchTitle: string;
+  order: SortKeyType;
+  page: number;
+  size: number;
+}
+export interface FavoriteQueryType extends BaseQueryType {
+  favorite: boolean;
+}
+export interface CategoryQueryType extends BaseQueryType {
+  category: typeof CATEGORY[number] | "전체";
+}
+export interface TagQueryType extends BaseQueryType {
+  tags: string[];
+}
+
+export type QueryType = FavoriteQueryType | CategoryQueryType | TagQueryType;
+
 export type OpenType = "private" | "all" | "partial";
 
 export type NotificationType = "SHARE" | "FEED" | "OLD";
