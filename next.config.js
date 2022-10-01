b9d7fcbee0d3595d08fdd1202058005286a201a7
YuHyun <puh0128@gmail.com>
2022-10-01 12:06:34 +0900

    [#403] chore: 소셜 로그인 관련 환경 변수 제거

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   env: {
     GOOGLE_ID: process.env.GOOGLE_ID,
-    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
-    GITHUB_ID: process.env.GITHUB_ID,
-    GITHUB_SECRET: process.env.GITHUB_SECRET,
-    KAKAO_ID: process.env.KAKAO_ID,
-    KAKAO_SECRET: process.env.KAKAO_SECRET,
     END_POINT: process.env.END_POINT,
   },
 };
