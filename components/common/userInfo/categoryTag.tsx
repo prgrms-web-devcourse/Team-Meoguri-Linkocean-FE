819774480149ef5c0bb5201e38cebdb351d29251
NamgyungKim <namgyung.kim@gmail.com>
2022-10-04 17:54:51 +0900

    [#411] refactor: category 상수 따로 빼기

--- components/common/userInfo/categoryTag.tsx
+++ components/common/userInfo/categoryTag.tsx
@@ -1,5 +1,6 @@
 import { color, text } from "@/styles/theme";
 import { CATEGORY } from "@/types/type";
+import { CATEGORY_MAP } from "@/utils/constants";
 import styled from "@emotion/styled";
 import React from "react";
 
@@ -11,7 +12,7 @@ const CategoryTag = ({ name }: CategoryTagProps) => {
   return (
     <Tag>
       <img
-        src={`/icon/category/${CATEGORY_MAP[name]}-selected.svg`}
+        src={`/icon/category/${CATEGORY_MAP[name].fileName}-selected.svg`}
         alt={name}
       />
       <span>{name}</span>
@@ -37,20 +38,5 @@ const Tag = styled.div`
     font-weight: bold;
   }
 `;
-
-const CATEGORY_MAP = {
-  자기계발: "self-development",
-  인문: "humanities",
-  정치: "politics",
-  사회: "social",
-  예술: "art",
-  과학: "science",
-  기술: "technology",
-  IT: "it",
-  가정: "home",
-  건강: "health",
-  여행: "travel",
-  요리: "cooking",
-};
 
 export default CategoryTag;
