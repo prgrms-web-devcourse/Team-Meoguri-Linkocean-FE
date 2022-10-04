957058c0370f68b49e952a1a2bfcaf20d37a0868
NamgyungKim <namgyung.kim@gmail.com>
2022-10-04 17:54:51 +0900

    [#411] refactor: category 상수 따로 빼기

--- utils/constants.ts
+++ utils/constants.ts
@@ -22,3 +22,18 @@ export const LINKOCEAN_PATH = {
   feedDetail: "/feed/detail",
   notFound: "/404",
 } as const;
+
+export const CATEGORY_MAP = {
+  자기계발: { color: "#7DCD97", fileName: "self-development" },
+  인문: { color: "#60A8D4", fileName: "humanities" },
+  정치: { color: "#FFC0CB", fileName: "politics" },
+  사회: { color: "#EDD05C", fileName: "social" },
+  예술: { color: "#E78565", fileName: "art" },
+  과학: { color: "#7B61FF", fileName: "science" },
+  기술: { color: "#3E4CA2", fileName: "technology" },
+  IT: { color: "#3E7B57", fileName: "it" },
+  가정: { color: "#DC6363", fileName: "home" },
+  건강: { color: "#516CF6", fileName: "health" },
+  여행: { color: "#82DD4A", fileName: "travel" },
+  요리: { color: "#966353", fileName: "cooking" },
+} as const;
