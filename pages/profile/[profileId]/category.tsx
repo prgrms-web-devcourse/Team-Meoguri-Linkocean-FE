56f7faaa0c607a0428fd69e55e08bbbb918e720b
YuHyun <puh0128@gmail.com>
2022-10-11 23:49:44 +0900

    [#423] refactor: 불필요한 코드 제거

--- pages/profile/[profileId]/category.tsx
+++ pages/profile/[profileId]/category.tsx
@@ -1,20 +1,13 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import {
-  PageLayout,
-  UserInfo,
-  OtherFilterMenu,
-  Meta,
-} from "@/components/common";
+import { PageLayout, Meta } from "@/components/common";
 import OtherBookmark from "@/components/otherBookmark/otherBookmarkTemplate";
 import profileAPI from "@/utils/apis/profile";
 import { ProfileDetail } from "@/types/model";
 
 const Category = () => {
   const router = useRouter();
-  const [tags, setTags] = useState<string[]>([]);
-  const [category, setCategory] = useState<string>();
+
   const [otherProfileInfo, setOtherProfileInfo] = useState<ProfileDetail>();
 
   const { profileId } = router.query;
@@ -35,23 +28,6 @@ const Category = () => {
       getOtherProfileApi(parseInt(profileId, 10));
     }
   }, [profileId]);
-
-  useEffect(() => {
-    const tagsString = tags === undefined ? "" : tags.join(",");
-    const tagParamsObj = { tags: tagsString };
-    const searchParams = new URLSearchParams(tagParamsObj).toString();
-    if (tags.length !== 0 && typeof profileId === "string") {
-      router.push(`/profile/${profileId}/tag/?${searchParams}`);
-    }
-  }, [tags]);
-
-  useEffect(() => {
-    const categoryParamsObj = category ? { category } : { category: "" };
-    const searchParams = new URLSearchParams(categoryParamsObj).toString();
-    if (category !== undefined && typeof profileId === "string") {
-      router.push(`/profile/${profileId}/category/?${searchParams}`);
-    }
-  }, [category]);
 
   return (
     <>
